4af613e9a8686bbf2d85c313e33dd67f825ebfb1
Rizki Ramadhan <rizukirimu@gmail.com>
2021-03-19 18:48:29 +0700

    Init Project

---
+++ src/screens/CreatePin/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CreatePin = () => {
+  return (
+    <View>
+      <Text>CreatePin</Text>
+    </View>
+  );
+};
+
+export default CreatePin;
